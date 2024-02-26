ba8b6c8ecdb78f9896780afc4ce0f8b78bc4a7ab
pushpank <pushpankq@gmail.com>
2024-02-26 23:35:23 +0530

    create basic Video Player app template and implement xcodeGen

---
+++ VideoPlayer/VideoPlayer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Pushpank Kumar on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
